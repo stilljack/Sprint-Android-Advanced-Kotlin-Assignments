2289a231d40ae1d605daa3f1162403713b8f6f93
jack seymour <stilljack@gmail.com>
2019-10-16 12:52:11 -0700

    init

---
+++ M03/assigment/app/src/main/java/com/saucefan/stuff/m03/Model.kt
@@ -0,0 +1,7 @@
+package com.saucefan.stuff.m03
+
+sealed class Model (name:String,artist:String) {
+    class Rock()
+    class Disco()
+    class Whatever()
+}
--- M03/guided/app/src/main/java/com/saucefan/stuff/guided/MarineMammals.kt
+++ M03/guided/app/src/main/java/com/saucefan/stuff/guided/MarineMammals.kt
@@ -1,6 +1,7 @@
 package com.saucefan.stuff.guided
 
-sealed class MarineMammals (val name:String)
-class whale(name:String):MarineMammals(name)
-class porpoise(name:String):MarineMammals(name)
-class Dolphin(name:String):MarineMammals(name)
+sealed class MarineMammals (val name:String) {
+    class whale(name: String) : MarineMammals(name)
+    class porpoise(name: String) : MarineMammals(name)
+    class Dolphin(name: String) : MarineMammals(name)
+}
