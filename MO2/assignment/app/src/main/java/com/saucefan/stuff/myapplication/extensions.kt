24e9e714f3e797dcd448679d49ac16fccb6f358d
jack seymour <stilljack@gmail.com>
2019-10-16 02:34:59 -0700

    hahaha i didn't have the context.getString(R.string.channelID) set everywhere... ok whatever i'm done with this exercise

--- MO2/assignment/app/src/main/java/com/saucefan/stuff/myapplication/extensions.kt
+++ MO2/assignment/app/src/main/java/com/saucefan/stuff/myapplication/extensions.kt
@@ -4,14 +4,12 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_DEFAULT
+import android.app.PendingIntent
 import android.content.Context
-import android.content.Context.BIND_IMPORTANT
 import android.content.Context.NOTIFICATION_SERVICE
-import android.content.res.Resources
-import android.graphics.BitmapFactory
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Build
-import android.provider.Settings.Global.getString
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
@@ -19,10 +17,9 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
-import javax.sql.DataSource
 
 
-const val NOTIF_ID =0
+const val NOTIF_ID =22
 lateinit var descriptionText:String
 //lateinit var importance:String
 
@@ -31,9 +28,12 @@ fun notificationChannelInitialize(context: Context){
         // Create the NotificationChannel
         val name = context.getString(R.string.channelName)
         descriptionText = context.getString(R.string.channelDescription)
-        //importance = getString(NotificationManager.IMPORTANCE_DEFAULT)
+        //importance =
         val mChannel = NotificationChannel(context.getString(R.string.channelID), name, NotificationManager.IMPORTANCE_LOW)
         mChannel.description = context.getString(R.string.channelDescription)
+        mChannel.importance=IMPORTANCE_DEFAULT
+        mChannel.name=name
+
         // Register the channel with the system; you can't change the importance
         // or other notification behaviors after this
         val notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
@@ -47,20 +47,28 @@ fun NotificationCompat.Builder.easy(context: Context, title:String, content:Stri
     val nm =context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
     val res = context.getResources()
-    val builder = NotificationCompat.Builder(context)
-
-    builder
+   // val builder = NotificationCompat.Builder(context)
+    val intent = Intent(context, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+    val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    val builder = NotificationCompat.Builder(context, context.getString(R.string.channelID))
         .setSmallIcon(R.drawable.ic_launcher_background)
-        .setLargeIcon(BitmapFactory.decodeResource(res, R.drawable.ic_launcher_background))
+       // .setLargeIcon(BitmapFactory.decodeResource(res, R.drawable.ic_launcher_background))
         .setTicker(res.getString(R.string.ticker))
         .setWhen(System.currentTimeMillis())
         .setAutoCancel(true)
         .setContentTitle(title)
+        .setDefaults(Notification.DEFAULT_LIGHTS or Notification.DEFAULT_SOUND)
         .setContentText(content)
-    val n = builder.build()
+        .setContentIntent(pendingIntent)
+        .setContentInfo("Info")
 
-    nm.notify(notificationID, n)
-}
+
+        nm.notify(NOTIF_ID, builder.build())
+    }
+
+
 
 
 fun ImageView.loadurl(context:Context,url: String,id:ImageView) {
