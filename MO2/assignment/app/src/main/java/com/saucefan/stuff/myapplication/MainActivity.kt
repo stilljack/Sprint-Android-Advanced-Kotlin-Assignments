ee6ec0216e8d381e54305140cad246adfbc88b9e
jack seymour <stilljack@gmail.com>
2019-10-16 02:36:00 -0700

    hahaha i didn't have the context.getString(R.string.channelID) set everywhere... ok whatever i'm done with this exercise

---
+++ MO2/assignment/app/src/main/java/com/saucefan/stuff/myapplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.saucefan.stuff.myapplication
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.saucefan.stuff.myapplication.ui.main.MainFragment
+import kotlinx.android.synthetic.main.main_fragment.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+
+
+
+
+    }
+
+}
---
+++ MO2/assignment/app/src/main/java/com/saucefan/stuff/myapplication/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.saucefan.stuff.myapplication.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
