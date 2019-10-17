dd7982cfedc73f6209a01d916c107b3f6479c3e0
jack seymour <stilljack@gmail.com>
2019-10-17 10:52:52 -0700

    this sucks, bring on the matrixies but leave me out of this uncessary opaque ass rehash of fragments

--- M04/goodMorning/app/src/main/java/com/saucefan/stuff/unrequestionreplacementforfragments/HomeController.kt
+++ M04/goodMorning/app/src/main/java/com/saucefan/stuff/unrequestionreplacementforfragments/HomeController.kt
@@ -5,16 +5,60 @@ import android.widget.TextView
 import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.widget.Button
 import androidx.annotation.NonNull
-import com.bluelinelabs.conductor.Controller
+import com.bluelinelabs.conductor.*
+import com.bluelinelabs.conductor.changehandler.HorizontalChangeHandler
 
 
 class HomeController : Controller() {
-
-    override fun onCreateView(@NonNull inflater: LayoutInflater, @NonNull container: ViewGroup): View {
+ val horizontalChangeHandler =HorizontalChangeHandler()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
         val view = inflater.inflate(R.layout.activity_main, container, false)
-        (view.findViewById(R.id.tv_title) as TextView).text = "Hello World"
+        (view.findViewById(R.id.tv_title) as TextView).text = "i'm da joker baby.txt backstack size =${router.backstackSize}"
         return view
+    }
+    protected override fun onChangeEnded(
+        changeHandler: ControllerChangeHandler,
+        changeType: ControllerChangeType
+    ) {
+        super.onChangeEnded(changeHandler, changeType)
+
+        setButtonsEnabled(true)
+
+        val btnView=view?.findViewById<Button>(R.id.btn)
+        val btnView2 =view?.findViewById<Button>(R.id.btn2)
+        btnView?.setOnClickListener {
+            router.pushController(RouterTransaction.with(HomeController())
+                .pushChangeHandler(HorizontalChangeHandler())
+                .popChangeHandler(HorizontalChangeHandler())
+            )
+
+        }
+        if (router.backstackSize == 0){
+            btnView2?.visibility =  INVISIBLE
+        }
+        btnView2?.setOnClickListener {
+            router.popCurrentController()
+        }
+    }
+
+    protected override fun onChangeStarted(
+        changeHandler: ControllerChangeHandler,
+        changeType: ControllerChangeType
+    ) {
+        super.onChangeStarted(changeHandler, changeType)
+
+        setButtonsEnabled(false)
+    }
+    private fun setButtonsEnabled(enabled: Boolean) {
+        val view = view
+        if (view != null) {
+            (enabled)
+        }
+
+
     }
 
 }
