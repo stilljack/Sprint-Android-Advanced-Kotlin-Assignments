a3aaf8752a7f7f35a6e529c34ede3482dca52467
jack seymour <stilljack@gmail.com>
2019-10-17 14:01:36 -0700

    ho hum technically we're communicating but that's not the goal. maybe we can do something more interesting... maybe

---
+++ M04/assign/app/src/main/java/com/saucefan/stuff/assignment/controllers/BaseCtrler.kt
@@ -0,0 +1,14 @@
+package com.saucefan.stuff.assignment.controllers
+
+import com.bluelinelabs.conductor.Controller
+
+
+abstract class BaseCtrler : Controller() {
+
+
+
+    //constructor(arg: Bundle) {
+
+    //}
+
+}
---
+++ M04/assign/app/src/main/java/com/saucefan/stuff/assignment/controllers/ChildController.kt
@@ -0,0 +1,39 @@
+package com.saucefan.stuff.assignment.controllers
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.bluelinelabs.conductor.Controller
+import com.bluelinelabs.conductor.ControllerChangeHandler
+import com.bluelinelabs.conductor.ControllerChangeType
+import com.saucefan.stuff.assignment.R
+
+class ChildController<T>() : BaseCtrler()
+   where T:Controller, T:ChildController.dataPassController
+{
+constructor(targetController: T): this()
+     {
+        setTargetController(targetController)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
+        return inflater.inflate(R.layout.content_main,container,false)
+    }
+interface dataPassController{
+    fun recieveMSG(int:Int)
+}
+
+    override fun onChangeEnded(
+        changeHandler: ControllerChangeHandler,
+        changeType: ControllerChangeType
+    ) {
+        super.onChangeEnded(changeHandler, changeType)
+        val sauce = view?.findViewById<Button>(R.id.btn2)
+        
+
+    }
+
+
+
+}
